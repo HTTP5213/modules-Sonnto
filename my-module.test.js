57287b1f0f0900bce718a85440453f305778e4d3
Anthony Ho <90372802+Sonnto@users.noreply.github.com>
2023-03-24 13:52:13 -0400

    add imports, exports; node.modules excluded

---
+++ my-module.test.js
@@ -0,0 +1,19 @@
+import { expect } from 'chai';
+
+import { HobbitSettlement } from '../public/another-module.js';
+
+import { theShire, statement } from '../public/my-module.js';
+
+describe('The Shire', function () {
+  it('should be Hobbit Settlement', function () {
+    expect(theShire).to.be.an.instanceof(HobbitSettlement);
+  });
+});
+
+describe('statement', function () {
+  it('Should return the correct string', () => {
+    expect(statement()).to.equal(
+      'The Shire was founded 739 years before Buckland.'
+    );
+  });
+});
